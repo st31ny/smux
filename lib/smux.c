8bfbcba3170680adfa6fc88d7ceadac5c0c7555a
Maximilian Stein <m@steiny.biz>
2017-05-21 21:50:54 +0200

    implement receive

--- lib/smux.c
+++ lib/smux.c
@@ -4,7 +4,7 @@
 // adjust ring buffer index
 #define ADJRBI(i, s) ((i) >= (s) ? (i)-(s) : (i))
 // used elements in the buffer (given head, tail and size)
-#define RBUSED(h, t, s) ((h)>=(t) ? (h)-(t)+1 : (s)-(t)+(h)+1)
+#define RBUSED(h, t, s) ((h)>=(t) ? (h)-(t) : (s)-(t)+(h))
 
 // protocol properties
 #define PROTO_CHANNEL_BYTES 1
@@ -18,7 +18,7 @@ void smux_init(struct smux_config *config)
     *config = zero_config;
 
     // non-zero stuff
-    config->proto.esc = 'X';
+    config->proto.esc = '\x01';
 }
 
 void smux_free(struct smux_config *config)
@@ -101,6 +101,106 @@ ssize_t smux_send(struct smux_config *config, smux_channel ch, const void *buf, 
 
 ssize_t smux_recv(struct smux_config *config, smux_channel *ch, void *buf, size_t count)
 {
+    char *read_buf = (char*)config->buffer.read_buf;
+    unsigned read_buf_head = config->_internal.read_buf_head;
+    unsigned read_buf_tail = config->_internal.read_buf_tail;
+    size_t read_buf_size = config->buffer.read_buf_size;
+    size_t read_buf_used = RBUSED(read_buf_head, read_buf_tail, read_buf_size);
+    smux_read_fn read_fn = config->buffer.read_fn;
+    int fd = config->buffer.read_fd;
+    smux_channel recv_ch = config->_internal.recv_ch; // current channel
+    size_t recv_chars = config->_internal.recv_chars; // remaining payload chars
+    char esc = config->proto.esc;
+
+    ssize_t ret = 1;
+    unsigned end;
+    char* output_buf = (char*)buf;
+    size_t count_copied = 0;
+
+    // enough data available or fetch?
+    while(read_buf_used == 0 || // buffer empty
+       (read_buf[read_buf_tail] == esc && read_buf_used == 1) || // incomplete escape sequence
+       (read_buf[read_buf_tail] == esc && read_buf[ADJRBI(read_buf_tail + 1, read_buf_size)] != 0 &&
+          read_buf_used < 1 + PROTO_CHANNEL_BYTES + PROTO_SIZE_BYTES) // incomplete ch esc seq
+    )
+    {
+        if(!read_fn)
+            return 0;
+
+        // leave one byte space to ensure separation of buffer full/empty
+        end = read_buf_tail <= read_buf_head ? read_buf_size : read_buf_tail - 1;
+        if(read_buf_tail == 0)
+            end = read_buf_size - 1;
+        ret = read_fn(fd, (void*)(read_buf + read_buf_head), end - read_buf_head);
+        if(ret <= 0)
+            break;
+
+        read_buf_head = ADJRBI(read_buf_head + ret, read_buf_size);
+        read_buf_used += ret;
+    }
+
+    config->_internal.read_buf_head = read_buf_head;
+    // on error, always return for propor error handling
+    // if ret == 0, not enough chars were received
+    if(ret <= 0)
+        return ret;
+
+    // read buffer byte wise
+    *ch = recv_ch;
+    while(count_copied < count && // caller buffer not full
+        (recv_ch == 0 || count_copied < recv_chars) && // we are still receiving for recv_ch
+        read_buf_head != read_buf_tail // receive buffer not empty
+    )
+    {
+        if(read_buf[read_buf_tail] == esc)
+        {
+            read_buf_tail = ADJRBI(read_buf_tail + 1, read_buf_size);
+
+            // another char to decode esc seq?
+            if(read_buf_tail == read_buf_head)
+                break;
+
+            if(read_buf[read_buf_tail] == 0) // just escape of esc char
+            {
+                output_buf[count_copied++] = esc;
+                read_buf_tail = ADJRBI(read_buf_tail + 1, read_buf_size);
+            } else // channel information
+            {
+                // enough to decode channel and size?
+                if(RBUSED(read_buf_head, read_buf_tail, read_buf_size) < PROTO_CHANNEL_BYTES + PROTO_SIZE_BYTES)
+                    break;
+
+                recv_ch = read_buf[read_buf_tail];
+                read_buf_tail = ADJRBI(read_buf_tail + 1, read_buf_size);
+                recv_chars = (read_buf[read_buf_tail] << 8) & 0xFF00;
+                read_buf_tail = ADJRBI(read_buf_tail + 1, read_buf_size);
+                recv_chars |= read_buf[read_buf_tail] & 0xFF;
+                read_buf_tail = ADJRBI(read_buf_tail + 1, read_buf_size);
+
+                if(count_copied > 0)
+                {
+                    // in case we already copied payload, stop to separate channels
+                    break;
+                } else
+                {
+                    // otherwise, report the channel
+                    *ch = recv_ch;
+                }
+            }
+        } else // normal case (no esc char)
+        {
+            output_buf[count_copied++] = read_buf[read_buf_tail];
+            read_buf_tail = ADJRBI(read_buf_tail + 1, read_buf_size);
+        }
+    }
+
+    // write indexes and receiver state back
+    config->_internal.read_buf_tail = read_buf_tail;
+    config->_internal.read_buf_head = read_buf_head;
+    config->_internal.recv_ch = recv_ch;
+    config->_internal.recv_chars = recv_chars;
+
+    return count_copied;
 }
 
 ssize_t smux_flush(struct smux_config *config)

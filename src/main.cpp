084795fad84b50ad9a6cc704442bee75e9846162
Maximilian Stein <m@steiny.biz>
2017-06-29 00:52:09 +0200

    implement main function for test bench

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,39 @@
 #include <unistd.h>
 #include <smux.hpp>
 
+#include "file_factory.h"
+#include "rt.h"
+
 int main(int argc, const char* argv[])
+{
+    using namespace smux_client;
+    auto fac = file_factory::get();
+
+    std::clog << "open master-in" << std::endl;
+    auto master_in = fac->create("stdin", file_mode::in, {});
+    std::clog << "open master-out" << std::endl;
+    auto master_out = fac->create("stdout", file_mode::out, {});
+
+    std::clog << "open ch0" << std::endl;
+    auto file0_out = fac->create("file", file_mode::out, {"file-ch0-out"});
+    auto file0_in = fac->create("file", file_mode::in, {"file-ch0-in"});
+
+    std::clog << "open ch1" << std::endl;
+    auto file1_out = fac->create("file", file_mode::out, {"file-ch1-out"});
+    auto file1_in = fac->create("file", file_mode::in, {"file-ch1-in"});
+
+    std::clog << "starting re..." << std::endl;
+
+    std::unique_ptr<runtime_system> rt(new runtime_system(std::move(master_in), std::move(master_out)));
+    rt->add_channel(0, std::move(file0_in), std::move(file0_out));
+    rt->add_channel(1, std::move(file1_in), std::move(file1_out));
+
+    rt->run();
+
+    return 0;
+}
+
+int main_smux(int argc, const char* argv[])
 {
     if(argc != 2 || (*argv[1] != 'e' && *argv[1] != 'd'))
     {

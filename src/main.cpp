cab8c6116f510a37e81abfc59fee9bddc9d3960b
Maximilian Stein <m@steiny.biz>
2017-10-20 21:56:36 +0200

    add help message

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,9 @@ static void setup_signals()
         std::cerr << "registering signal handler failed: " << std::strerror(errno) << std::endl;
 }
 
+void print_help(std::ostream& os, char const* pgrm_name);
+
+
 int main(int argc, const char* argv[])
 {
     using namespace smux_client;
@@ -69,6 +72,11 @@ int main(int argc, const char* argv[])
     {
         std::cerr << "parsing configuration failed: " << e.what() << std::endl;
         exit(EXIT_FAILURE);
+    }
+    if(conf->help_level())
+    {
+        print_help(std::clog, argv[0]);
+        return 0;
     }
     print_config(std::clog, *conf);
 
@@ -157,4 +165,42 @@ static std::unique_ptr<smux_client::runtime_system> load_rt(smux_client::cnf con
     }
 
     return rt;
+}
+
+void print_help(std::ostream& os, char const* pgrm_name)
+{
+    os << "Usage:\n"
+        << "(1) " << pgrm_name << " --master :<file definition> {<channel definition>}\n"
+        << "(2) " << pgrm_name << " [--master i:<file definition>] [--master o:<file definition>] {<channel definition>}\n"
+        << "(3) " << pgrm_name << " -h\n\n"
+        << "In form (1), use the same <file definition> for reading and writing multiplexed\n"
+        << "data. Form (2) allows using a different file for input and for output. Omitting\n"
+        << "either definition is possible to only read or write multiplexed data.\n"
+        << "Form (3) prints this help message.\n"
+        << "\nChannel definition syntax:\n"
+        << "    <channel number>:[(i|o)]:<file definition>\n"
+        << "The channel number must be in the range 0..255\n"
+        << "Specifying either 'i' or 'o' allows the creation of different files for reading (i) and writing (o)\n"
+        << "data. Omitting 'i' and 'o' opens the file for reading and writing.\n\n"
+        << "\nFile definition syntax:\n"
+        << "    <file type>:<arg 1>:<arg 2>:...\n"
+        << "Depending on the file type arguments are mandatory or optional\n\n"
+        << "File types:\n"
+        << "  stdio                         Read from stdin and write to stdout\n"
+        << "  file:<file name>[:(a|t)]      Open the file <file name> for reading/writing (depending on channel definition)\n"
+        << "                                with open(2). Optionally, the argument 'a' sets the 'O_APPEND' flag and 't'\n"
+        << "                                the 'O_TRUNC' flag. When opening for reading and writing or for writing only,\n"
+        << "                                'O_CREAT' is set.\n"
+        << "  exec:<program command line>   Executes the specified program and uses stdin and stdout for communication\n"
+        << "                                (depending on read/write mode in channel definition only one of the two).\n"
+        << "                                The stdout of the program is used as input for the specified channel and data\n"
+        << "                                received on a specific channel is forwarded to the stdin of the program.\n"
+        << "  socat:<address>               Wrapper for 'exec' to use with socat. Depending on the read/write mode of\n"
+        << "                                the channel translates to one of the three file definitions:\n"
+        << "                                   'exec:socat -d -d stdin <address>' (channel is in write only)\n"
+        << "                                   'exec:socat -d -d stdout <address>' (channel is read only)\n"
+        << "                                   'exec:socat -d -d stdio <address>' (channel is read/write)\n"
+        << "                                For details about the usage of socat, refer to its manpage.\n"
+        << "\n\nI know, the syntax is very cumbersome. Got a better idea of interface design? Please let me know!\n"
+        << std::flush;
 }

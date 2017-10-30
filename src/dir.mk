605c33458fe4f3f112194ab97d9fb26e8eae3045
Maximilian Stein <m@steiny.biz>
2017-10-30 20:00:57 +0100

    add test dummy to successfully run tests

--- src/dir.mk
+++ src/dir.mk
@@ -3,7 +3,9 @@
 
 MYDIR                   := $(dir $(lastword $(MAKEFILE_LIST)))
 
-SRC_CXX                 := main.cpp file_factory.cpp files.cpp rt.cpp cnf.cpp cnf_argv.cpp \
+SRC_CXX                 := file_factory.cpp files.cpp rt.cpp cnf.cpp cnf_argv.cpp \
                            debug.cpp
+SRC_CXX_main            := main.cpp
+SRC_CXX_test            := test_dummy.cpp
 
 include $(BUILDIR)/mk/dir.mk
---
+++ src/test_dummy.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void)
+{
+    std::clog << "No tests implemented yet..." << std::endl;
+    return 0;
+}
